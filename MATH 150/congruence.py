ef5b719c1a108cc6a229668afe51f0d524110e4a
Evan Gartley <evanmgartley@gmail.com>
2021-06-21 19:32:42 -0400

    Remove math courses

--- MATH 150/congruence.py
+++
@@ -1,9 +0,0 @@
-print("a = b % n")
-a = int(input("a: "))
-b = int(input("b: "))
-n = int(input("n: "))
-
-if a % n == b % n:
-    print("True, since they both have the same modulous result")
-else:
-    print("False, since they have different modulous results")
--- MATH 150/quotient_remainder.py
+++
@@ -1,9 +0,0 @@
-def get_q_r(m, n):
-    q = m // n
-    r = m - (n * q)
-    return [q, r]
-
-if __name__ == "__main__":
-    m = int(input("m: "))
-    n = int(input("n: "))
-    print(get_q_r(m, n))
